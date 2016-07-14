f4ef3330a7b21cfa1868e3516e5c0e9aa3c7bf0b
Daniel Carr <d.douglas.carr@gmail.com>
2016-07-15 08:59:31 +1000

    spring initializr output

---
+++ src/test/java/org/rooset/server/RoosetServerApplicationTests.java
@@ -0,0 +1,18 @@
+package org.rooset.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RoosetServerApplication.class)
+@WebAppConfiguration
+public class RoosetServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
